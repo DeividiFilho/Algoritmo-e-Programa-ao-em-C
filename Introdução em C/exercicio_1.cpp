be7db7c75b096f77bfd7132386ead50825505dc2
Deividi Filho <82412889+DeividiFilho@users.noreply.github.com>
2023-03-05 20:45:08 -0300

    Update exercicio_1.cpp

--- Introdução em C/exercicio_1.cpp
+++ Introdução em C/exercicio_1.cpp
@@ -1,1 +1,14 @@
-reggg
+// 1- Verdadeiro ou falso:
+// a) 1 > 2 
+// b) ‘a’ < ‘b’
+// c) 3 == 2 
+// d)‘1’ == ‘1’
+// e) 3 >= 2 
+// f)‘j’ != ‘j’
+
+// a) Falso
+// b) Verdadeiro
+// c) Falso
+// d) Verdadeiro
+// e) Verdadeiro
+// f) Falso
