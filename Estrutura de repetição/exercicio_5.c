b5fb6af8b5772e0a1aaf228e9806725612002c03
Deividi Filho <82412889+DeividiFilho@users.noreply.github.com>
2023-03-05 21:27:15 -0300

    Rename exercicio_5..c to exercicio_5.c

--- Estrutura de repetição/exercicio_5.c
+++ Estrutura de repetição/exercicio_5.c
@@ -1,14 +1,14 @@
 //5- Fazer um programa que leia um conjunto de valores
-//correspondentes ‡s notas que alunos obtiveram em
-//uma prova. Quando o valor fornecido for um n˙mero
-//negativo, significa que n„o existem mais pontos para
-//serem lidos. ApÛs isso seu programa dever·:
+//correspondentes √†s notas que alunos obtiveram em
+//uma prova. Quando o valor fornecido for um n√∫mero
+//negativo, significa que n√£o existem mais pontos para
+//serem lidos. Ap√≥s isso seu programa dever√°:
 //	
-//a. Escrever quantas notas s„o maiores ou iguais a
+//a. Escrever quantas notas s√£o maiores ou iguais a
 //6.0
-//b. Escrever quantas notas s„o maiores ou iguais a
+//b. Escrever quantas notas s√£o maiores ou iguais a
 //4.0 e menores que 6.0
-//c. Escrever quantos notas s„o menores que 4.0
+//c. Escrever quantos notas s√£o menores que 4.0
 
 #include <stdio.h>
 
@@ -17,10 +17,10 @@ int main() {
 
     // Loop para ler as notas
     while (1) {  // Loop infinito
-        printf("Digite a nota (ou um n˙mero negativo para sair): ");
+        printf("Digite a nota (ou um n√∫mero negativo para sair): ");
         scanf("%d", &nota);
 
-        // Verifica se o usu·rio digitou um n˙mero negativo para sair do loop
+        // Verifica se o usu√°rio digitou um n√∫mero negativo para sair do loop
         if (nota < 0) {
             break;  // Sai do loop
         }
