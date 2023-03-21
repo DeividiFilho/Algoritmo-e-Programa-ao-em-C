7cedb516ca8487e63fd98d21926a742fdc736180
Deividi Filho <82412889+DeividiFilho@users.noreply.github.com>
2023-03-20 22:01:24 -0300

    Update Ead-Busca-Sequencial-Binaria.c

--- EAD - Busca Binária e Sequencial/Ead-Busca-Sequencial-Binaria.c
+++ EAD - Busca Binária e Sequencial/Ead-Busca-Sequencial-Binaria.c
@@ -1,90 +1,90 @@
 // Deividi Douglas de Carlo FIlho
 //	
 //
-//  ATIVIDADE 2 DO CONTE⁄DO 1 ñ
-// AULA EAD SOBRE M…TODOS DE BUSCA
-// SEQUENCIAL E BIN¡RIA 
+//  ATIVIDADE 2 DO CONTE√öDO 1 ‚Äì
+// AULA EAD SOBRE M√âTODOS DE BUSCA
+// SEQUENCIAL E BIN√ÅRIA 
 
-// DescriÁ„o do exercÌcio:
-// FaÁa um programa em C que crie um vetor com 500 n˙meros inteiros gerados
-// aleatoriamente. Em seguida, o programa dever· solicitar um valor do usu·rio a
-// ser buscado no vetor e verificar se o mesmo pertence ou n„o ao vetor,
-// imprimindo a mensagem ao usu·rio.
-// O programa dever· implementar duas formas de busca no vetor: sequencial e
-// bin·ria. Para realizar a busca bin·ria, vocÍ dever· criar o seu prÛprio algoritmo
-// de ordenaÁ„o. 
+// Descri√ß√£o do exerc√≠cio:
+// Fa√ßa um programa em C que crie um vetor com 500 n√∫meros inteiros gerados
+// aleatoriamente. Em seguida, o programa dever√° solicitar um valor do usu√°rio a
+// ser buscado no vetor e verificar se o mesmo pertence ou n√£o ao vetor,
+// imprimindo a mensagem ao usu√°rio.
+// O programa dever√° implementar duas formas de busca no vetor: sequencial e
+// bin√°ria. Para realizar a busca bin√°ria, voc√™ dever√° criar o seu pr√≥prio algoritmo
+// de ordena√ß√£o. 
 
 
 
 //               RESUMO
 // Pensei em fazer de duas formas esse programa:
-// Fazer o usu·rio escolher entre a busca sequencial ou bin·ria
+// Fazer o usu√°rio escolher entre a busca sequencial ou bin√°ria
 // ou fazer e mostrar o resultado da busca sequencial e binaria junto (escolhi esse)
-// mas caso deseje fazer com a primeira opÁ„o È sÛ usar Switch case.
+// mas caso deseje fazer com a primeira op√ß√£o √© s√≥ usar Switch case.
 
-// Usarei conceitos aprendido posteriormente em aula, como funÁıes, ponteiros!
-// Esse cÛdigo ser· postado no GitHub afim educacionais e para demonstrar meus avanÁos na linguagem C
-// Qualque duvida ou erro encontrado, entre em contato via gmail: deividi.filho@aluno.ifsp.edu.br (institucional
-// deividifilhom2017@gmail.com (gmail pessoal) ou www.linkedin.com/in/Deividi-Filho
+// Usarei conceitos aprendido posteriormente em aula, como fun√ß√µes, ponteiros!
+// Esse c√≥digo ser√° postado no GitHub afim educacionais e para demonstrar meus avan√ßos na linguagem C
+// Qualque duvida ou erro encontrado, entre em contato via gmail: deividi.filho@aluno.ifsp.edu.br (institucional)
+// ou www.linkedin.com/in/Deividi-Filho
 
 
-// Usaremos 3 funÁıes. Uma para "sortear", outra para fazer a busca sequencial e a outra para busca bin·ria
-// Desculpe pelo excesso de coment·rio, mas estou fazendo o mais did·tico possÌvel
-// pq essa È a melhor forma que achei de aprender. Ensinando outra pessoa.
+// Usaremos 3 fun√ß√µes. Uma para "sortear", outra para fazer a busca sequencial e a outra para busca bin√°ria
+// Desculpe pelo excesso de coment√°rio, mas estou fazendo o mais did√°tico poss√≠vel
+// pq essa √© a melhor forma que achei de aprender. Ensinando outra pessoa.
 
-#include <stdio.h>  // biblioteca padr„o do C 
-#include <stdlib.h> // biblioteca padr„o do C que define funÁıes e macros para gerenciamento de memÛria, convers„o de tipos de dados
-#include <time.h>  //  biblioteca padr„o do C que fornece funÁıes para manipulaÁ„o de data e hora.  utilizada neste programa para gerar uma semente aleatÛria para a funÁ„o rand.
+#include <stdio.h>  // biblioteca padr√£o do C 
+#include <stdlib.h> // biblioteca padr√£o do C que define fun√ß√µes e macros para gerenciamento de mem√≥ria, convers√£o de tipos de dados
+#include <time.h>  //  biblioteca padr√£o do C que fornece fun√ß√µes para manipula√ß√£o de data e hora.  utilizada neste programa para gerar uma semente aleat√≥ria para a fun√ß√£o rand.
 #include <locale.h> // biblioteca para poder usar o idioma PT-Br
 
-// FunÁ„o que ordena o vetor usando o algoritmo bubble sort
+// Fun√ß√£o que ordena o vetor usando o algoritmo bubble sort
 
-// Uma breve explicaÁ„o desse algoritmo: 
-// È um algoritmo de ordenaÁ„o dos mais simples. A ideia È percorrer o vector diversas vezes, 
-// e a cada passagem fazer flutuar para o topo o maior elemento da sequÍncia Fonte: Wilipedia 
+// Uma breve explica√ß√£o desse algoritmo: 
+// √© um algoritmo de ordena√ß√£o dos mais simples. A ideia √© percorrer o vector diversas vezes, 
+// e a cada passagem fazer flutuar para o topo o maior elemento da sequ√™ncia Fonte: Wilipedia 
 
 // Os comentarios explicativo de cada linha foi feitor com a ajuda do chatGPT 3
 
 void bubbleSort(int *vetor, int tamanho) {
 	int i, j, aux;
-	for (i=0; i<tamanho-1; i++) { // como o indice do vetor comeÁa em 0, È preciso usar o "-1" para pegar o ultimo indice
-		for (j=0; j<tamanho-i-1; j++) { // apÛs cada passagem pelo vetor, o maior elemento j· estar· na sua posiÁ„o final. (Percorre atÈ a penultima posiÁ„o n„o ordenada
-			if(vetor[j] > vetor[j+1]) { // realizam a troca de posiÁ„o de dois elementos adjacentes no vetor, caso o elemento na posiÁ„o atual j seja maior do que o elemento na posiÁ„o seguinte j+1. 
-				aux  = vetor[j]; // A vari·vel aux È usada para armazenar temporariamente o valor do elemento na posiÁ„o atual vetor[j]
-				vetor[j] = vetor[j+1]; // sobrescrevÍ-lo com o valor do prÛximo elemento vetor[j+1]. O valor do prÛximo elemento È atribuÌdo ‡ posiÁ„o atual vetor[j]
-				vetor[j+1] = aux; // e o valor armazenado em aux È atribuÌdo ‡ posiÁ„o seguinte vetor[j+1]
+	for (i=0; i<tamanho-1; i++) { // como o indice do vetor come√ßa em 0, √© preciso usar o "-1" para pegar o ultimo indice
+		for (j=0; j<tamanho-i-1; j++) { // ap√≥s cada passagem pelo vetor, o maior elemento j√° estar√° na sua posi√ß√£o final. (Percorre at√© a penultima posi√ß√£o n√£o ordenada
+			if(vetor[j] > vetor[j+1]) { // realizam a troca de posi√ß√£o de dois elementos adjacentes no vetor, caso o elemento na posi√ß√£o atual j seja maior do que o elemento na posi√ß√£o seguinte j+1. 
+				aux  = vetor[j]; // A vari√°vel aux √© usada para armazenar temporariamente o valor do elemento na posi√ß√£o atual vetor[j]
+				vetor[j] = vetor[j+1]; // sobrescrev√™-lo com o valor do pr√≥ximo elemento vetor[j+1]. O valor do pr√≥ximo elemento √© atribu√≠do √† posi√ß√£o atual vetor[j]
+				vetor[j+1] = aux; // e o valor armazenado em aux √© atribu√≠do √† posi√ß√£o seguinte vetor[j+1]
 			}
 		}
 	}
 }
 
-// ExplicaÁ„o dessa funÁ„o: (Pelo chatGPT)
-// A implementaÁ„o do bubble sort na funÁ„o bubbleSort È feita com dois laÁos for aninhados. O laÁo externo percorre o vetor inteiro, da posiÁ„o inicial atÈ a pen˙ltima posiÁ„o, 
-// e o laÁo interno percorre o vetor da posiÁ„o inicial atÈ a posiÁ„o final n„o ordenada. A cada iteraÁ„o do laÁo interno, o algoritmo compara o elemento atual vetor[j] com o 
-// prÛximo elemento vetor[j+1]. Se vetor[j] for maior do que vetor[j+1], os elementos s„o trocados de posiÁ„o usando uma vari·vel auxiliar aux. Isso È repetido atÈ que todo o vetor esteja ordenado.
+// Explica√ß√£o dessa fun√ß√£o: (Pelo chatGPT)
+// A implementa√ß√£o do bubble sort na fun√ß√£o bubbleSort √© feita com dois la√ßos for aninhados. O la√ßo externo percorre o vetor inteiro, da posi√ß√£o inicial at√© a pen√∫ltima posi√ß√£o, 
+// e o la√ßo interno percorre o vetor da posi√ß√£o inicial at√© a posi√ß√£o final n√£o ordenada. A cada itera√ß√£o do la√ßo interno, o algoritmo compara o elemento atual vetor[j] com o 
+// pr√≥ximo elemento vetor[j+1]. Se vetor[j] for maior do que vetor[j+1], os elementos s√£o trocados de posi√ß√£o usando uma vari√°vel auxiliar aux. Isso √© repetido at√© que todo o vetor esteja ordenado.
 //
-// Dessa forma, ao final da execuÁ„o da funÁ„o, o vetor passado como par‚metro ser· ordenado em ordem crescente.
+// Dessa forma, ao final da execu√ß√£o da fun√ß√£o, o vetor passado como par√¢metro ser√° ordenado em ordem crescente.
 
-// FunÁ„o que realiza a busca sequencial no vetor
+// Fun√ß√£o que realiza a busca sequencial no vetor
 
 int buscaSequencial(int *vetor, int tamanho, int valor) {
 	int i;
-	for(i=0; i<tamanho; i++) {  // Percorrendo o vetor por padr„o
-		if(vetor[i] == valor) { // Verifica se o valor que vocÍ procura est· no vetor
+	for(i=0; i<tamanho; i++) {  // Percorrendo o vetor por padr√£o
+		if(vetor[i] == valor) { // Verifica se o valor que voc√™ procura est√° no vetor
 			return 1;           // Valor encontrado 
 		}
 	}
-	return 0;                  // Valor n„o encontrado
+	return 0;                  // Valor n√£o encontrado
 }
 
-// ExplicaÁ„o dessa funÁ„o: (Pelo chatGPT)
-// A funÁ„o buscaSequencial implementa o algoritmo de busca sequencial em um vetor de inteiros. Ele recebe como par‚metros um vetor de inteiros vetor, o tamanho do vetor tamanho
+// Explica√ß√£o dessa fun√ß√£o: (Pelo chatGPT)
+// A fun√ß√£o buscaSequencial implementa o algoritmo de busca sequencial em um vetor de inteiros. Ele recebe como par√¢metros um vetor de inteiros vetor, o tamanho do vetor tamanho
 // e o valor que se deseja buscar valor.
 // O algoritmo de busca sequencial percorre o vetor da esquerda para a direita, comparando cada elemento do vetor com o valor buscado. Se o elemento for igual ao valor buscado, 
-// a busca È interrompida e a funÁ„o retorna o valor 1, indicando que o valor foi encontrado. Caso contr·rio, a busca continua atÈ o final do vetor.
+// a busca √© interrompida e a fun√ß√£o retorna o valor 1, indicando que o valor foi encontrado. Caso contr√°rio, a busca continua at√© o final do vetor.
 
 
-// FunÁ„o que realiza a busca bin·ria no vetor
+// Fun√ß√£o que realiza a busca bin√°ria no vetor
 
 int buscaBinaria(int *vetor, int tamanho, int valor){
 	int inicio = 0;
@@ -92,82 +92,82 @@ int buscaBinaria(int *vetor, int tamanho, int valor){
 	int meio; 
 	while (inicio <= fim) {
 		meio = (inicio + fim) / 2; // "Calcula" o meio do vetor
-		if (vetor[meio] == valor) { // Verifica se o meio do vetor È o n˙mero sendo procurado
+		if (vetor[meio] == valor) { // Verifica se o meio do vetor √© o n√∫mero sendo procurado
 			return 1;
 		} else if (vetor[meio] < valor){
-			inicio = meio + 1;    // Verifica se o meio È menor do que o n˙mero procurado (o valor procurado est· a direita)
+			inicio = meio + 1;    // Verifica se o meio √© menor do que o n√∫mero procurado (o valor procurado est√° a direita)
 		} else {
-			fim = meio - 1;      // Verifica se o meio È maior do que o n˙mero procurado (o valor procurado est· a esquerda)
+			fim = meio - 1;      // Verifica se o meio √© maior do que o n√∫mero procurado (o valor procurado est√° a esquerda)
 		}
 	}
-	return 0;                   // Valor n„o encontrado	
+	return 0;                   // Valor n√£o encontrado	
 }
 
-// ExplicaÁ„o dessa funÁ„o: (Pelo chatGPT)
-// A funÁ„o buscaBinaria implementa o algoritmo de busca bin·ria em um vetor de inteiros. Ele recebe como par‚metros um vetor de inteiros vetor, o tamanho do vetor tamanho 
+// Explica√ß√£o dessa fun√ß√£o: (Pelo chatGPT)
+// A fun√ß√£o buscaBinaria implementa o algoritmo de busca bin√°ria em um vetor de inteiros. Ele recebe como par√¢metros um vetor de inteiros vetor, o tamanho do vetor tamanho 
 // e o valor que se deseja buscar valor.
-// inicialmente definimos as vari·veis inicio e fim, que representam o intervalo em que vamos procurar o valor. No inÌcio, o intervalo È todo o vetor, por isso inicio È definido como 0 
-// e fim È definido como tamanho-1.
-// Em seguida, temos um laÁo while que È executado enquanto o intervalo de busca n„o se reduzir a zero. A cada iteraÁ„o do laÁo, o algoritmo calcula o Ìndice do meio do intervalo meio 
-// como a mÈdia entre inicio e fim. Em seguida, o algoritmo verifica se o valor buscado est· no elemento do meio vetor[meio]. Se o valor for encontrado, a funÁ„o retorna o valor 1 indicando
+// inicialmente definimos as vari√°veis inicio e fim, que representam o intervalo em que vamos procurar o valor. No in√≠cio, o intervalo √© todo o vetor, por isso inicio √© definido como 0 
+// e fim √© definido como tamanho-1.
+// Em seguida, temos um la√ßo while que √© executado enquanto o intervalo de busca n√£o se reduzir a zero. A cada itera√ß√£o do la√ßo, o algoritmo calcula o √≠ndice do meio do intervalo meio 
+// como a m√©dia entre inicio e fim. Em seguida, o algoritmo verifica se o valor buscado est√° no elemento do meio vetor[meio]. Se o valor for encontrado, a fun√ß√£o retorna o valor 1 indicando
 // que o valor foi encontrado.
-// Se o valor buscado n„o estiver no elemento do meio, o algoritmo verifica se o valor buscado È maior ou menor que o elemento do meio. Se o valor buscado for maior que o elemento do meio, 
-// significa que o valor procurado est· na metade direita do intervalo, ent„o o algoritmo redefine inicio como meio + 1. Caso contr·rio, o valor procurado est· na metade esquerda do intervalo, 
-// ent„o o algoritmo redefine fim como meio - 1.
+// Se o valor buscado n√£o estiver no elemento do meio, o algoritmo verifica se o valor buscado √© maior ou menor que o elemento do meio. Se o valor buscado for maior que o elemento do meio, 
+// significa que o valor procurado est√° na metade direita do intervalo, ent√£o o algoritmo redefine inicio como meio + 1. Caso contr√°rio, o valor procurado est√° na metade esquerda do intervalo, 
+// ent√£o o algoritmo redefine fim como meio - 1.
 
 
-int main(){ // FunÁ„o main È a funÁ„o principal. Um tipo de ponto de partida
-    setlocale(LC_ALL,"Portuguese"); // estamos definindo a localizaÁ„o para Portugal 
+int main(){ // Fun√ß√£o main √© a fun√ß√£o principal. Um tipo de ponto de partida
+    setlocale(LC_ALL,"Portuguese"); // estamos definindo a localiza√ß√£o para Portugal 
 	
 	int vetor[5000];
 	int i, valor, encontradoSeq, encontradoBin;
 	
-	srand(time(NULL)); // Gera os 500 n˙meros aleatÛrios. OBS: Precisei de uma ajuda do chatGPT pq estava dando erro e n„o encontrei a soluÁ„o
+	srand(time(NULL)); // Gera os 500 n√∫meros aleat√≥rios. OBS: Precisei de uma ajuda do chatGPT pq estava dando erro e n√£o encontrei a solu√ß√£o
 	for(i=0; i<50; i++){
-		vetor[i] = rand() % 1000; // Gera n˙meros aleatÛrio de 1 a 1000;
+		vetor[i] = rand() % 1000; // Gera n√∫meros aleat√≥rio de 1 a 1000;
 	}
 	
-	bubbleSort(vetor, 500); // Ordena o vetor com o Bubble sort, a primeira funÁ„o criada
+	bubbleSort(vetor, 500); // Ordena o vetor com o Bubble sort, a primeira fun√ß√£o criada
 	
 	printf("\n Digite um valor a ser buscado no vetor: "); // Solicita um valor para ser buscado
 	scanf("%d", &valor);
 	
-	encontradoSeq = buscaSequencial(vetor, 500, valor); // Realiza a busca sequencial (Chama a funÁ„o e passa os paramÍtros)
+	encontradoSeq = buscaSequencial(vetor, 500, valor); // Realiza a busca sequencial (Chama a fun√ß√£o e passa os param√™tros)
 	if (encontradoSeq) {
 		printf("\n O valor %d foi encontrado no vetor usando o algoritmo de busca SEQUENCIAL", valor);
 	} else {
-		printf("\n O valor %d N√O foi encontrado no vetor usando o algoritmo de busca SEQUENCIAL", valor);
+		printf("\n O valor %d N√ÉO foi encontrado no vetor usando o algoritmo de busca SEQUENCIAL", valor);
 	}
 	
-	encontradoBin = buscaBinaria(vetor, 500, valor); // Realiza a busca sequencial (Chama a funÁ„o e passa os paramÍtros)
+	encontradoBin = buscaBinaria(vetor, 500, valor); // Realiza a busca sequencial (Chama a fun√ß√£o e passa os param√™tros)
 	if (encontradoBin){
-		printf("\n O valor %d foi encontrado no vetor usando o algoritmo de busca BIN¡RIA", valor);
+		printf("\n O valor %d foi encontrado no vetor usando o algoritmo de busca BIN√ÅRIA", valor);
 	} else {
-		printf("\n O valor %d N√O foi encontrado no vetor usando o algoritmo de busca BIN¡RIA", valor);
+		printf("\n O valor %d N√ÉO foi encontrado no vetor usando o algoritmo de busca BIN√ÅRIA", valor);
 	}
 	return 0;
 }
 
-// ExplicaÁ„o dessa funÁ„o: (Pelo chatGPT)
-// a primeira coisa que È feita È definir a localizaÁ„o para o Brasil, com suporte a caracteres UTF-8, usando a funÁ„o setlocale.
-// Em seguida, È criado um vetor de 500 inteiros e È utilizado a funÁ„o srand(time(NULL)) para inicializar a semente do gerador de
-// n˙meros aleatÛrios, garantindo que cada vez que o programa for executado, os n˙meros gerados ser„o diferentes. Ent„o, s„o gerados
-//  500 n˙meros aleatÛrios de 1 a 1000 usando a funÁ„o rand().
+// Explica√ß√£o dessa fun√ß√£o: (Pelo chatGPT)
+// a primeira coisa que √© feita √© definir a localiza√ß√£o para o Brasil, com suporte a caracteres UTF-8, usando a fun√ß√£o setlocale.
+// Em seguida, √© criado um vetor de 500 inteiros e √© utilizado a fun√ß√£o srand(time(NULL)) para inicializar a semente do gerador de
+// n√∫meros aleat√≥rios, garantindo que cada vez que o programa for executado, os n√∫meros gerados ser√£o diferentes. Ent√£o, s√£o gerados
+//  500 n√∫meros aleat√≥rios de 1 a 1000 usando a fun√ß√£o rand().
 //
-// Depois, È utilizado o algoritmo Bubble Sort para ordenar o vetor gerado. Em seguida, o programa solicita que o usu·rio digite um valor
+// Depois, √© utilizado o algoritmo Bubble Sort para ordenar o vetor gerado. Em seguida, o programa solicita que o usu√°rio digite um valor
 // a ser buscado no vetor.
 //
-// A funÁ„o buscaSequencial È chamada para realizar a busca sequencial no vetor. Se o valor for encontrado, a mensagem 
-// "O valor X foi encontrado no vetor usando o algoritmo de busca SEQUENCIAL" È impressa na tela. Caso contr·rio, a mensagem 
-// "O valor X N√O foi encontrado no vetor usando o algoritmo de busca SEQUENCIAL" È exibida.
+// A fun√ß√£o buscaSequencial √© chamada para realizar a busca sequencial no vetor. Se o valor for encontrado, a mensagem 
+// "O valor X foi encontrado no vetor usando o algoritmo de busca SEQUENCIAL" √© impressa na tela. Caso contr√°rio, a mensagem 
+// "O valor X N√ÉO foi encontrado no vetor usando o algoritmo de busca SEQUENCIAL" √© exibida.
 //
-// A funÁ„o buscaBinaria È chamada para realizar a busca bin·ria no vetor. Se o valor for encontrado, a mensagem 
-// "O valor X foi encontrado no vetor usando busca BIN¡RIA" È impressa na tela. Caso contr·rio, a mensagem 
-// "O valor X N√O foi encontrado no vetor usando busca BIN¡RIA" È exibida.
-// Por fim, a funÁ„o main retorna 0, indicando que o programa foi executado com sucesso.
+// A fun√ß√£o buscaBinaria √© chamada para realizar a busca bin√°ria no vetor. Se o valor for encontrado, a mensagem 
+// "O valor X foi encontrado no vetor usando busca BIN√ÅRIA" √© impressa na tela. Caso contr√°rio, a mensagem 
+// "O valor X N√ÉO foi encontrado no vetor usando busca BIN√ÅRIA" √© exibida.
+// Por fim, a fun√ß√£o main retorna 0, indicando que o programa foi executado com sucesso.
 
-// Fiz pouco teste, aparentemente quando escolhe o valor "0" para ser procurado sempre È encontrado. 
-// Provavelmente um "bug" que precisa ser corrigido. Apenas testar e dizer que n„o pode ser 0. 
+// Fiz pouco teste, aparentemente quando escolhe o valor "0" para ser procurado sempre √© encontrado. 
+// Provavelmente um "bug" que precisa ser corrigido. Apenas testar e dizer que n√£o pode ser 0. 
 
 
 
